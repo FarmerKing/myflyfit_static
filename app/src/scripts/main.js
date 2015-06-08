708301b5a37bc83855e7945d6f3fab3ec54ea582
FarmerKing <king_chen@mail2000.com.tw>
2015-06-08 17:04:03 +0800

    add app for react module

---
+++ app/src/scripts/main.js
@@ -0,0 +1,10 @@
+
+require('../assets/stylesheets/style.less');
+
+// TODO: Require assets here.
+// require('../assets/images/product.png');
+
+import App from './components/App.js';
+import React from 'react';
+
+React.render(<App />, document.getElementById('main'));
